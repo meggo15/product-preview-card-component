d40d9080e4d8ca06735c0dea9b406538a395abdf
Mary <99505177+meggo15@users.noreply.github.com>
2022-09-09 17:42:34 -0700

    Added flex flow to content wrapper in css

---
+++ main.js
@@ -0,0 +1,6 @@
+const button = document.QuerySelector ("button");
+
+button.addEventListener  ("click", function () {
+    button.classList.remove ("button")
+    button.classList.add ("button-hover")
+});
